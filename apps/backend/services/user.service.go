6e15a70354b8828375a6165ab6998ca2eb0c2bcd
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-08 04:55:34 +0530

    Feat : added jwt token

--- apps/backend/services/user.service.go
+++ apps/backend/services/user.service.go
@@ -6,10 +6,11 @@ import (
 	"time"
 
 	"github.com/ANU7MADHAV/algo-arena/models"
+	"github.com/golang-jwt/jwt/v5"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -17,6 +18,7 @@ type AuthService struct {
 	collection *mongo.Collection
 	jwtSecret string
 }
+
 
 func NewAuthService (db *mongo.Database, jwtSecret string) *AuthService {
 return &AuthService{
@@ -26,7 +28,7 @@ return &AuthService{
 }
 
 func (s *AuthService) CreateUser(user *models.User) (*models.User,error) {
-	ctx,cancel := context.WithTimeout(context.Background(),100*time.Millisecond)
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
 	defer cancel()
 
 	existingUser := &models.User{}
@@ -66,4 +68,56 @@ return nil, errors.New("user with this email or username already exists")
 	user.Password = ""
 
 	return user,nil
+}
+
+
+func (s *AuthService) LoginUser(credentials *models.User) (*models.AuthResponse,error) {
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+
+	defer cancel()
+
+	user := &models.User{}
+
+	err := s.collection.FindOne(ctx,bson.M{
+		"$or" : []bson.M {
+			{"email" : user.Email},
+			{"username" : user.Username},
+		},
+	}).Decode(user)
+
+	if err != nil {
+		if err != mongo.ErrNoDocuments {
+			return nil,err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(credentials.Password))
+	if err != nil {
+		return nil,errors.New("Invalid credentials")
+	}
+
+	token,err := s.generateJWT(user)
+
+	if err != nil {
+		return nil,err
+	}
+
+	user.Password = ""
+
+	return &models.AuthResponse {
+		User: user,
+		Token: token,
+	},nil
+}
+
+func (s *AuthService) generateJWT (user *models.User) (string,error){
+	claims := jwt.MapClaims{
+"id" : user.ID,
+"username" : user.Username,
+"email" : user.Email,
+"role" : user.Role,
+"exp" : time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	return token.SignedString([]byte(s.jwtSecret))
 }
