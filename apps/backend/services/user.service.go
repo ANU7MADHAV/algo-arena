2a852aa4f0e14298c52c41a90d48513dbbb159d8
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-08 02:41:51 +0530

    Feat : initilized development branch

--- apps/backend/services/user.service.go
+++ apps/backend/services/user.service.go
@@ -1,7 +1,13 @@
 package services
 
 import (
+	"context"
+	"errors"
+	"time"
+
 	"github.com/ANU7MADHAV/algo-arena/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,5 +24,21 @@ return &AuthService{
 }
 
 func (s *AuthService) CreateUser(user *models.User) (*models.User,error) {
-	
+	ctx,cancel := context.WithTimeout(context.Background(),100*time.Millisecond)
+	defer cancel()
+
+	existingUser := &models.User{}
+	err := s.collection.FindOne(ctx,bson.M{
+		"$or" : []bson.M {
+			{"email" : user.Email},
+			{"username" : user.Username},
+		},
+	}).Decode(existingUser)
+
+	if err != nil && err != mongo.ErrNoDocuments {
+		return nil,err
+	}
+	if existingUser.ID != primitive.NilObjectID {
+return nil, errors.New("user with this email or username already exists")
+	}
 }
