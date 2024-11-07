e7ceb6248427bc33a236c05f703509a4e00afa04
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-08 02:53:41 +0530

    Feat : added register user service

--- apps/backend/services/user.service.go
+++ apps/backend/services/user.service.go
@@ -9,6 +9,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -41,4 +43,27 @@ func (s *AuthService) CreateUser(user *models.User) (*models.User,error) {
 	if existingUser.ID != primitive.NilObjectID {
 return nil, errors.New("user with this email or username already exists")
 	}
+
+	hashedPassword,err := bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.DefaultCost)
+
+	if err != nil {
+		return nil,err
+	}
+
+	user.Password = string(hashedPassword)
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	user.Role = "user"
+
+	result,err := s.collection.InsertOne(ctx,user)
+
+	if err != nil {
+		return nil,err
+	}
+	user.ID = result.InsertedID.(primitive.ObjectID)
+
+
+	user.Password = ""
+
+	return user,nil
 }
