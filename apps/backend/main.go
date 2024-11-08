8daaca0c7bda5e72e764406f88b8c2daefa7daae
ANU7MADHAV <anumadhavan888@gmail.com>
2024-11-08 20:07:44 +0530

    Feat : http server initialised

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -1,11 +1,55 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ANU7MADHAV/algo-arena/config"
+	"github.com/ANU7MADHAV/algo-arena/delivery/http"
+	domain "github.com/ANU7MADHAV/algo-arena/domain/usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	server      *gin.Engine
+	us          domain.UserUseCase
+	uc          http.UserController
+	ctx         context.Context
+	mongoClient *mongo.Client
+)
+
+func init() {
+	ctx = context.TODO()
+
+	// mongo
+
+	mongoClient, err := config.Connect()
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println("Mongo connected")
+	us := domain.NewUserUseCase(mongoClient)
+	uc = http.NewUserController(us)
+
+	server = gin.Default()
+
+}
+
 func main() {
-  r := gin.Default()
- routes.setUp()
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	defer func(mongoClient *mongo.Client, ctx context.Context) {
+		err := mongoClient.Disconnect(ctx)
+		if err != nil {
+			fmt.Println("error")
+			return
+		}
+	}(mongoClient, ctx)
+	basepath := server.Group("/v1")
+	uc.RegisterRoutes(basepath)
+
+	log.Fatal(server.Run(":3000"))
 }
